a2ce36e3fc85644a64ce3546123a88c9e3f17ba7
Brent <brentw.white@gmail.com>
2015-05-14 10:59:23 -1000

    keyboardEvents 4:15

--- keyboardEvents.js
+++ keyboardEvents.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     });
 });
 
-
+// 4.14
 // Within our event handler, update the number of nights in the `#nights-count` element to whatever the traveler entered in the `#nights` form field.
 
 $(document).ready(function() {
@@ -17,8 +17,26 @@ $(document).ready(function() {
 
         // finding the `nights-count` id then using the `text()` method
         // to access the value from value within the form with the
-        // `val()` method. 
+        // `val()` method.
         $('#nights-count').text($(this).val());
 
+    });
+});
+
+
+// 4.15
+//  Set the content of the `#total` element to the number the traveler has entered into the form field multiplied by the daily price.
+
+$(document).ready(function(){
+    $('.tour').on('keyup',function(){
+        $('#nights-count').text($(this).val());
+        // setting a varaible that contains the dailyPrice rate
+        var dailyPrice =+$(this).cloeset('.tour').data('daily-price');
+        // setting a varaible that contains the number of nights
+        // the user wishes to stay
+        var nights =+$(this).val();
+
+        // multiplying dailyPrice and nights to get the full rate
+        $('#total').text(dailyPrice * nights);    
     });
 });
