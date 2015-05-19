c3966d2673f3148ec71249463e57ed4af9a3a06e
Brent <brentw.white@gmail.com>
2015-05-18 14:28:14 -1000

    link layover complete

--- ListeningTo_DOM_Events/js/linkLayover.js
+++ ListeningTo_DOM_Events/js/linkLayover.js
@@ -18,3 +18,34 @@ $(document).ready(function(){
 });
 
 // 4.20
+$(document).ready(function(){
+    // adding a link event parameter
+    $('.see-photos').on('click',function(event){
+        // stopPropagination prevents any parent handlers from being
+        // notified of the event
+        event.stopPropagination();
+        // once the link is clicked show the photos for the clicked tour
+        $(this).closest('.tour').find('.photos').slideToggle();
+    });
+
+    // this event should be silenced forever more!
+    $('.tour').on('click',function(){
+        alert('this must not be called!!');
+    });
+});
+
+// 4.21
+$(document).ready(function(){
+    $('.see-photos').on('click',function(event){
+        // preventDefault fixes our page jumping issue
+        event.preventDefault();
+        // stopPropagation stops our second event from being fired.
+        event.stopPropagation();
+        $(this).closest('.tour').find('.photos').slideToggle();
+    });
+
+    // this event will not be fired 
+    $('.tour').on('click',function(event){
+        alert('This should not be called!');
+    });
+});
